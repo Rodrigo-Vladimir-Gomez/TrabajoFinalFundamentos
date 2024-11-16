f199db439f74d555efcb076b2248dc43873a3db6
Rodrigo-Vladimir-Gomez <GA19038@ues.edu.sv>
2024-11-15 19:34:41 -0600

    Base de datos

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 #Creamos la conexion ala base de datos
-engine=create_engine("mariadb+pymysql://root:rodrigo@localhost:3307/proyectogasolineras")
+engine=create_engine("postgresql://root:XItt9pnTL9HRKHNYMsvr53iVaSgcNuK4@dpg-csrvc3btq21c739pu8v0-a.oregon-postgres.render.com/pruebagas_k20d")
 #creamos la metadata para la creacion de los datos
 meta=MetaData()
 #almacenamos la conexion para utilizarla
