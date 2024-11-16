6d2dab2f60bee57abdfbd7ce9726581337e7afe6
Rodrigo Vladimir Gómez Aguilar <115365953+Rodrigo-Vladimir-Gomez@users.noreply.github.com>
2024-11-15 20:57:42 -0600

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 #Creamos la conexion ala base de datos
-engine=create_engine("postgresql://root:XItt9pnTL9HRKHNYMsvr53iVaSgcNuK4@dpg-csrvc3btq21c739pu8v0-a.oregon-postgres.render.com/pruebagas_k20d")
+engine=create_engine("postgresql://root:LAx5J73uFh3lcIjLbvS6ZcOPM3oX53Me@dpg-css0jiq3esus739gbbf0-a.oregon-postgres.render.com/gasolineria")
 #creamos la metadata para la creacion de los datos
 meta=MetaData()
 #almacenamos la conexion para utilizarla
